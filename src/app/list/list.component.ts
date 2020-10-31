ecb095d0c29e0a2b78434456a3fc76ac5e932f1b
itizidon <dng21@binghamton.edu>
2020-10-31 17:46:49 -0400

    can add unselected providers and redirects you

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -6,9 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-
+  public selectedToggle = true;
+  public tempValues = [];
   public selectedProviders = [];
-  public providerMap = {};
   public unselectedProviders = [
     {
       id: '1',
@@ -30,16 +30,51 @@ export class ListComponent implements OnInit {
     }
   ];
   public test: number = 1
-  constructor() {}
+  constructor() { }
 
   ngOnInit() {
+    if (localStorage.getItem('selectedProviders')) {
+      this.selectedToggle = JSON.parse(localStorage.getItem('selectedToggle'))
+      this.selectedProviders = JSON.parse(localStorage.getItem('selectedProviders'))
+    }
   }
 
-  addProvider(provider){
-    if(!this.providerMap[provider.id]){
-    this.providerMap[provider.id] = this.selectedProviders.length
-    this.selectedProviders.push(provider)
+  changeToggle() {
+    if (this.selectedToggle === true) {
+      for (let i of this.tempValues) {
+        if (i !== false && !this.selectedProviders.includes(i)) {
+          this.selectedProviders.push(i)
+        }
+      }
     }
+    else {
+      for (let i = 0; i < this.selectedProviders.length; i++) {
+        if (this.tempValues.includes(this.selectedProviders[i])) {
+          this.selectedProviders.splice(i, 1)
+        }
+      }
+    }
+    this.tempValues = []
+    this.selectedToggle = !this.selectedToggle
+    localStorage.setItem('selectedToggle', JSON.stringify(this.selectedToggle))
+
+    localStorage.setItem('tempValues', JSON.stringify(this.tempValues))
+    localStorage.setItem('selectedProviders', JSON.stringify(this.selectedProviders))
+  }
+
+  addProvider(provider, checked) {
+    if (checked) {
+      this.tempValues.push(provider)
+    }
+    else {
+      for (let selectedTempValue = 0; selectedTempValue < this.tempValues.length; selectedTempValue++) {
+        if (this.tempValues[selectedTempValue] === provider) {
+          this.tempValues[selectedTempValue] = false
+        }
+      }
+
+    }
+
   }
 
 }
